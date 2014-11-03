57b262fabfd89163c5dc0f6cddb3aa80a7bfbc9b
badolit009 <badolit009@gmail.com>
2014-11-03 12:57:36 +0600

    EmployeeSalaryLib Initially Uploaded

---
+++ EmployeeSalaryLib/EmployeeSalaryLib/Salary.cs
@@ -0,0 +1,46 @@
+﻿namespace EmployeeSalaryLib
+{
+    public class Salary
+    {
+        public double Total { get; set; }
+        public double Basic { get; set; }
+        public double MedicalPercent { get; set; }
+        public double ConveyancePercent { get; set; }
+        public double IncrementPercent { get; set; }
+        public int NoOfIncrement { get; set; }
+
+        public Salary(double basic, double medicalPercent, double conveyancePercent, int incrementPercent):this()
+        {
+            Basic = basic;
+            MedicalPercent = medicalPercent;
+            ConveyancePercent = conveyancePercent;
+            IncrementPercent = incrementPercent;
+        }
+
+        public Salary()
+        {
+        }
+
+        public double GetTotal()
+        {
+           Total = Basic + GetMedicalPercent() + GetConveyancePercent()+GetIncrimentPercent();
+            return Total;
+        }
+
+        public double GetMedicalPercent()
+        {
+            return (Basic*MedicalPercent/100);
+        }
+
+        public double GetConveyancePercent()
+        {
+            return (Basic*ConveyancePercent/100);
+        }
+
+        public double GetIncrimentPercent()
+        {
+            NoOfIncrement++;
+            return (IncrementPercent*Basic/100);
+        }
+    }
+}
---
+++ EmployeeSalaryLib/EmployeeUnitTest.test/SalaryTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeSalaryLib;
+using NUnit.Framework;
+
+namespace EmployeeUnitTest.test
+{
+    [TestFixture]
+    class SalaryTest
+    {
+        private Salary aSalary;
+
+        [SetUp]
+        public void Start()
+        {
+            
+        }
+        [Test]
+        public void CreateSalaryAndCheckInitialyTotalZero()
+        {
+            aSalary = new Salary();
+            Assert.AreEqual(0,aSalary.Total);
+        }
+
+        [Test]
+        public void CreateSalaryWithBasic5000MedicaAndConveyanceTotalShouldBe6700()
+        {
+            aSalary = new Salary();
+            aSalary.Basic = 5000;
+            aSalary.MedicalPercent = 20;
+            aSalary.ConveyancePercent = 15;
+
+            Assert.AreEqual(6750,aSalary.GetTotal());
+        }
+
+        [Test]
+        public void CreateSalaryWithBasic5000AndMedicalPercent20()
+        {
+            aSalary = new Salary();
+            aSalary.Basic = 5000;
+            aSalary.MedicalPercent = 20;
+            Assert.AreEqual(1000,aSalary.GetMedicalPercent());
+        }
+
+        [Test]
+        public void CreateSalaryWithBasic5000AndConveyance15()
+        {
+            aSalary = new Salary();
+            aSalary.Basic = 5000;
+            aSalary.ConveyancePercent = 15;
+            Assert.AreEqual(750,aSalary.GetConveyancePercent());
+        }
+
+        [Test]
+        public void CreateSalaryWithBasic5000AndIncrementPercent20()
+        {
+            aSalary = new Salary();
+            aSalary.Basic = 5000;
+            aSalary.IncrementPercent = 20;
+            Assert.AreEqual(1000,aSalary.GetIncrimentPercent());
+        }
+        [Test]
+        public void CheckTotal()
+        {
+            aSalary = new Salary(5000, 20, 15, 20);
+            Assert.AreEqual(7750,aSalary.GetTotal());
+
+        }
+
+        [TearDown]
+        public void End()
+        {
+            aSalary = null;
+        }
+    }
+}
